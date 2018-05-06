9828efa6d0fd65affd86717ea4346f7065353b56
Luke Ivie <luke.ivie@gmail.com>
2018-05-05 20:56:20 -0700

    Added utils and styles and scss and all that

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import logo from '../../assets/logo.svg';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <Container fluid className="py-3 bg-white d-flex px-5 flew-row">
+        <img src={logo} alt="" className="d-flex" style={{maxWidth: "175px"}}/>
+      </Container>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Header from './components/Header/Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import 'bootstrap/dist/css/bootstrap.css';
+import "./styles/styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <h1>Big Heart Consulting</h1>
+        <h2>We help you help animals</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />
+, document.getElementById('root'));
