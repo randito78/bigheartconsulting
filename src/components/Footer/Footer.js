698eafb3b4b679905aa85495cd5068f76c1e284d
Randy Brookins <randyto78@gmail.com>
2019-01-05 12:28:51 -0700

    fix warnings

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { NavLink } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
